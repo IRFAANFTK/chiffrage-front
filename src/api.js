5ba92b92d8fa358ffff9bbada903eb2e4696ea9d
Rujbally Irfaan <i.rujbally@xefi.mu>
2025-06-05 15:20:06 +0200

    prod-my-ip

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:80/api', // Laravel API URL
+    baseURL: 'http://10.49.255.184:80/api', // Laravel API URL
 });
 
 export default api;
